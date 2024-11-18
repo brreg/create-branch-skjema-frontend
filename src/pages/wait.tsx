0294cc4e67010a8feb83952f2e53ab07b042a5a5
Øyvind Hatland <oyvind.hatland@yahoo.com>
2024-11-18 14:00:33 +0100

    more informative wait page

--- src/pages/wait.tsx
+++ src/pages/wait.tsx
@@ -6,23 +6,28 @@ import styles from '@/styles/wait.module.css'
 
 export default function Wait() {
   const [textIndex, setTextIndex] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [finishedArray, setFinishedArray] = useState(new Array(4).fill(false))
+  const [activeArray, setActiveArray] = useState([true, false, false, false])
   const router = useRouter()
 
   const texts = [
-    'Laster inn data fra lommeboken',
-    'Gjør avansert prosessering',
-    'Selger sensitive data til Trump',
-    'Gjør hverdagen din bedre'
+    'Kobler til lommeboken',
+    'Henter NPID',
+    'Henter EUCC',
+    'Lagrer data og forbreder skjema'
   ]
 
   useEffect(() => {
     const textInterval = setInterval(() => {
-      setTextIndex((prevIndex) => (prevIndex + 1) % texts.length)
+      setTextIndex((textIndex) => textIndex + 1)
+
     }, 2000)
 
     const navigationTimeout = setTimeout(() => {
       router.push('/skjema')
-    }, 7800)
+    }, 9000)
 
     return () => {
       clearInterval(textInterval)
@@ -30,12 +35,56 @@ export default function Wait() {
     }
   }, [router])
 
+  useEffect(() => {
+    const nextFinishedArray = finishedArray.map((b, i) => {
+      if (i < textIndex) {
+        return true
+      } else {
+        return false
+      }
+    })
+    setFinishedArray(nextFinishedArray)
+
+    const nextActiveArray = finishedArray.map((b, i) => {
+      if (i === textIndex) {
+        return true
+      } else {
+        return false
+      }
+    })
+    setActiveArray(nextActiveArray)
+  },[textIndex])
+
   return (
     <main className={styles.main}>
-      <div>
-        <Spinner title={texts[textIndex]} color='accent' data-size='xl' />
-        <p>{texts[textIndex]}</p>
+      <div className={styles.main2}>
+        <div className={styles.maincontent}>
+          {
+            texts.map((s, i) => {
+              return <StatusRow key={i} text={s} finished={finishedArray[i]} active={activeArray[i]} />
+            })
+          }
+        </div>
       </div>
     </main>
+  )
+}
+
+function StatusRow(props: any) {
+  const { text, finished, active } = props;
+
+  return (
+    <div className={styles.statusrow}>
+      <div className={styles.statusicon} >
+        {finished ?
+          <p>✅</p> : null
+        }
+        {active ?
+          <Spinner title={text} color="accent"
+            data-size="sm" /> : null
+        }
+      </div>
+      <p>{text}</p>
+    </div>
   )
 }
